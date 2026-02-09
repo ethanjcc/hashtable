5e80170616b8cbb3e0434c8c7819272782d110e6
U-BSD\453370 <453370@SHS-HYBF6X3.beaverton.k12.or.us>
2026-02-09 13:09:22 -0800

    commit three

--- hash.cpp
+++ hash.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <cstring>
+#include "node.h"
+#include "student.cpp"
 
 //create table and set size
 const int initTableSize = 100;
 Node** table;
 int tableSize;
+int hashFunc(int id);
+int chainLen(Node* head);
+void insertStudent(Student* s, Node** table);
+void rehash(int tableSize);
 
 int main(){
   tableSize = initTableSize;
@@ -12,29 +18,48 @@ int main(){
   for (int i = 0; i < tableSize; i++) {
     table[i] = nullptr;
   }
-  //hash function from copilot
-  int hashFunc(int id, int tableSize) {
-    return id % tableSize;
+}
+
+//hash function from copilot
+int hashFunc(int id) {
+  return id % tableSize;
+}
+//create length of chain
+int chainLen(Node* head) {
+  int len = 0;
+  Node* cur = head;
+  //add to length when a node is added
+  while (cur != nullptr) {
+    len++;
+    cur = cur->getNext();
   }
-  //create length of chain
-  int chainLen(Node* head) {
-    int len = 0;
-    Node* cur = head;
-    //add to length when a node is added
-    while (cur != nullptr) {
-      len++;
-      cur = cur->getNext;
-    }
+  return len;
+}
+//put student into the table
+void insertStudent(Student* s, Node** table) {
+  int index = hashFunc(s->id);
+  Node* newNode = new Node(s);
+  newNode->setNext(table[index]);
+  table[index] = newNode;
+  int len = chainLen(table[index]);
+  if (len > 3) {
+    rehash(tableSize);
   }
-  //put student into the table
-  void insertStudent(Student* s) {
-    int index = hashFunc(s->id);
-    Node* newNode = new Node(s);
-    newNode->setNext(table[index]);
-    table[index] = newNode;
-    int len = chainLen(table[index]);
-    if (len > 3) {
-      rehash;
+}
+void rehash(int tableSize) {
+  //make new larger table
+  int oldSize = tableSize;
+  int newSize = oldSize * 2;
+  Node** newTable = new Node*[newSize];
+  for (int i = 0; i < newSize; i++) {
+    newTable[i] = nullptr;
+  }
+  //add nodes from old table to new table
+  for (int i = 0; i < oldSize; i++) {
+    while (table[i] != nullptr){
+      insertStudent(table[i]->getStudent(), newTable);
+      table[i] = table[i]->getNext();
     }
   }
 }
+
---
+++ student.cpp
@@ -0,0 +1,12 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+//makes struct for student
+struct Student {
+  public:
+   char fname[20];
+   char lname[20];
+   int id;
+   float gpa;
+};
+#endif
